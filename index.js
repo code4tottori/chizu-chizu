62d0b4f5ea3b4847fca21ce7319b9fe33e36e1e0
Masayuki Higashino <msyk@hgsn.info>
2018-03-10 11:52:17 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function entry_to_url(entry) {
+  var base = 'https://maps.googleapis.com/maps/api/streetview';
+  var size = '?size=300x300';
+  var location = '&location=' + entry.lat + ',' + entry.lng;
+  var heading = '&heading=' + entry.heading;
+  var pitch = '&pitch=' + entry.pitch;
+  return base + size + location + heading + pitch;
+};
+(function() {
+  Papa.parse('markers.csv', {
+    download: true,
+    header: true,
+    delimiter: ',',
+    skipEmptyLines: true,
+    complete: function(params, url) {
+      params.data.forEach(function(entry) {
+        var url = entry_to_url(entry);
+        var tag = '<div class="card"><img class="card-img-top" src="' + url + '"><div class="card-body"><h5 class="card-title">' + entry.title + '</h5><p class="card-text">' + entry.description + '</p></div></div>'
+
+        $('.card-columns').append(tag)
+      });
+    }
+  })
+})();
